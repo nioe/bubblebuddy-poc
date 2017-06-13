e997856b0b27b777f398021daddcd2aee2f4ce9e
Nicolas Oeschger <noe@exq.ch>
2017-06-13 16:14:49 +0200

    Calling Spring Boot rest service to get data

--- react-poc/src/logBookEntries/LogBookEntries.js
+++ react-poc/src/logBookEntries/LogBookEntries.js
@@ -2,35 +2,21 @@ import React, {Component} from 'react';
 import './LogBookEntries.css';
 
 class LogBookEntries extends Component {
-    entries = [
-        {
-            "id": "593e9f085baab32db31a4bfd",
-            "diveDate": "2017-06-12T16:02:48.605",
-            "bottomTime": "PT51M",
-            "diveSite": "Gaia Corner",
-            "depth": 21.3,
-            "visibility": 10
-        },
-        {
-            "id": "593e9f975baab32db31a4bfe",
-            "diveDate": "2017-06-12T16:05:33",
-            "bottomTime": "PT48M",
-            "diveSite": "Ron's Reef",
-            "depth": 24,
-            "visibility": 8
-        },
-        {
-            "id": "593eadbb5baab33842815437",
-            "diveDate": "2017-06-12T16:53:32",
-            "bottomTime": "PT48M20S",
-            "diveSite": "Sapi Sloce",
-            "depth": 15.3,
-            "visibility": 12
-        }
-    ];
 
+    constructor(props) {
+        super(props);
+        this.state = {entries: []};
+    }
+
+    fetchLogBookEntries() {
+        return fetch('/logbook')
+            .then(response => response.json())
+            .catch(error => console.log('Could not fetch logbook entries...', error));
+    }
 
     render() {
+        this.fetchLogBookEntries().then(result => this.setState({entries: result}));
+
         return (
             <div className="logBookEntries">
                 <table className="table table-hover">
@@ -45,7 +31,7 @@ class LogBookEntries extends Component {
                     </thead>
                     <tbody>
                     {
-                        this.entries.map((dive, i) => {
+                        this.state.entries.map((dive, i) => {
                             let diveDate = new Date(Date.parse(dive.diveDate));
 
                             return (
